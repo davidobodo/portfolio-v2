696d50a1179f2d239baaff8245b95f479db8bfe9
Obodo David <obododavid5@gmail.com>
2022-10-06 22:05:51 +0100

    trigger redeploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { createGlobalStyle } from 'styled-components';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Home from './pages/home';
-import About from './pages/about';
-import Projects from './pages/projects';
-import Contact from './pages/contact';
+import Home from "./pages/home";
+import About from "./pages/about";
+import Projects from "./pages/projects";
+import Contact from "./pages/contact";
 
 const GlobalStyles = createGlobalStyle`
   *, 
@@ -29,23 +29,22 @@ const GlobalStyles = createGlobalStyle`
     font-size: 70px;
     font-weight: 600;
   }
-`
+`;
 
 const App = () => {
-  return (
-    <>
-      <GlobalStyles />
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/projects" component={Projects} />
-          <Route exact path="/contact" component={Contact} />
-        </Switch>
-      </Router>
-    </>
-
-  )
-}
+	return (
+		<>
+			<GlobalStyles />
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/about" component={About} />
+					<Route exact path="/projects" component={Projects} />
+					<Route exact path="/contact" component={Contact} />
+				</Switch>
+			</Router>
+		</>
+	);
+};
 
 export default App;
